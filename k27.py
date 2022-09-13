29a05b683fbdeb0ecff12989b6081f912e611b8b
Rityil <112727303+sabiyadr@users.noreply.github.com>
2022-09-13 14:36:14 -0500

    Create k27.py

---
+++ k27.py
@@ -0,0 +1,46 @@
+from stanfordkarel import *
+
+
+def main():
+    """ Karel code goes here! """
+    turn_left()
+    move()
+    move()
+    move()
+    move()
+    move()
+    move()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    pick_beeper()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    pick_beeper()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    pick_beeper()
+    move()
+    turn_left()
+    move()
+    pick_beeper()
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
