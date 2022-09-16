f877a7f1c2970bba6d8b8b301fff81ffc098bd02
Rityil <112727303+sabiyadr@users.noreply.github.com>
2022-09-16 14:09:13 -0500

    Create k29.py

---
+++ k29.py
@@ -0,0 +1,52 @@
+from stanfordkarel import *
+
+
+def main():
+    """ Karel code goes here! """
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    move()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
